f7bb1c75e65db5a2681203027025abc01ffe4e49
Sara Morillon <sara.morillon.pro@gmail.com>
2025-05-03 23:28:52 +0200

    Remove restriction on mp4

--- src/controllers/listRecords.ts
+++ src/controllers/listRecords.ts
@@ -10,15 +10,13 @@ export async function listRecords(req: IncomingMessage, res: ServerResponse) {
   const files = await readdir(env.RECORD_DIR)
   const records = []
   for (const file of files) {
-    if (file.endsWith('mp4')) {
-      const { size } = await stat(join(env.RECORD_DIR, file))
-      records.push({
-        id: file,
-        name: file.slice(10, -8),
-        size: prettyBytes(size),
-        href: `/record/${file}`,
-      })
-    }
+    const { size } = await stat(join(env.RECORD_DIR, file))
+    records.push({
+      id: file,
+      name: file.slice(10, -8),
+      size: prettyBytes(size),
+      href: `/record/${file}`,
+    })
   }
   res.setHeader('content-type', 'text/html')
   const template = handlebars.compile(readFileSync('templates/index.hbs', 'utf8'))
