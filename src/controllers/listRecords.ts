f371d2a5b371ab6d4535e0423d9d80010ca51b22
Sara Morillon <sara.morillon.pro@gmail.com>
2025-05-04 12:50:11 +0200

    Add free space indicator

--- src/controllers/listRecords.ts
+++ src/controllers/listRecords.ts
@@ -1,4 +1,5 @@
 import handlebars from 'handlebars'
+import { exec } from 'node:child_process'
 import { readFileSync } from 'node:fs'
 import { readdir, stat } from 'node:fs/promises'
 import type { IncomingMessage, ServerResponse } from 'node:http'
@@ -7,6 +8,19 @@ import prettyBytes from 'pretty-bytes'
 import { env } from '../env.js'
 
 export async function listRecords(req: IncomingMessage, res: ServerResponse) {
+  const size = await new Promise((resolve) =>
+    exec(`df -h ${env.RECORD_DIR}`, (err, stdout) => {
+      if (err) {
+        resolve('- B')
+      } else {
+        try {
+          resolve(stdout.split(/\s+/)[10])
+        } catch {
+          resolve('- B')
+        }
+      }
+    }),
+  )
   const files = await readdir(env.RECORD_DIR)
   const records = []
   for (const file of files) {
@@ -20,5 +34,5 @@ export async function listRecords(req: IncomingMessage, res: ServerResponse) {
   }
   res.setHeader('content-type', 'text/html')
   const template = handlebars.compile(readFileSync('templates/index.hbs', 'utf8'))
-  res.text(template({ records }))
+  res.text(template({ size, records }))
 }
